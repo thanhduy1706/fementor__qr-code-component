b3c4303241c6f0c23fa5c456b63e2fc42b27b7ec
Nguyen Duy <thanhduy1706@gmail.com>
2024-06-04 02:38:21 +0700

    chore: Update .gitignore to ignore node_modules and .DS_Store files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-gray": "hsl(212, 45%, 89%)",
+        "grayish-blue": "hsl(220, 15%, 55%)",
+        "dark-blue": "hsl(218, 44%, 22%)",
+      },
+      fontWeight: {
+        heading: "700",
+        body: "400",
+      },
+    },
+  },
+  plugins: [],
+}
